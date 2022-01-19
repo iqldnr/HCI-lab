56a7b03ce9f01b6634e79a51260cd7557679cbdd
iqldnr <82568791+iqldnr@users.noreply.github.com>
2022-01-20 00:13:07 +0700

    js
    jsjsjsjssjs

---
+++ script.js
@@ -0,0 +1,13 @@
+let a1 = document.getElementById("a1");
+let a2 = document.getElementById("a2");
+let h1 = document.getElementById("h1");
+
+a1.addEventListener("mouseover", () => {
+  window.location.href = "https://twitter.com";
+});
+a2.addEventListener("mouseover", () => {
+  window.location.href = "https://instagram.com";
+});
+h1.addEventListener("mouseover", () => {
+  window.location.href = "./index.html";
+});
