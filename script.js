04484ad2b7058f323be7d4011c270715b4c47f9b
iqldnr <82568791+iqldnr@users.noreply.github.com>
2022-01-20 04:43:43 +0700

    about
    dah good about

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ let a1 = document.getElementById("a1");
 let a2 = document.getElementById("a2");
 let h1 = document.getElementById("h1");
 
-a1.addEventListener("mouseover", () => {
+a1.addEventListener("click", () => {
   window.location.href = "https://twitter.com";
 });
-a2.addEventListener("mouseover", () => {
+a2.addEventListener("click", () => {
   window.location.href = "https://instagram.com";
 });
-h1.addEventListener("mouseover", () => {
+h1.addEventListener("click", () => {
   window.location.href = "./index.html";
 });
